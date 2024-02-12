1eea0cec770383eeb763936be8e81c44ea7fe48d
Pedro Henrique Bérgamo <pedroh.bergamo20@gmail.com>
2024-02-12 12:24:06 -0300

    📅 feat: Aula 03 Java

--- certification/src/main/java/dev/pedrohb/certification/modules/students/controllers/StudentController.java
+++ certification/src/main/java/dev/pedrohb/certification/modules/students/controllers/StudentController.java
@@ -1,12 +1,15 @@
 package dev.pedrohb.certification.modules.students.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.pedrohb.certification.modules.students.dtos.StudentCertificationAnswerDTO;
 import dev.pedrohb.certification.modules.students.dtos.VerifyHasCertificationDTO;
+import dev.pedrohb.certification.modules.students.useCases.StudentCertificationAnswersUseCase;
 import dev.pedrohb.certification.modules.students.useCases.VerifyHasCertificationUseCase;
 
 
@@ -15,6 +18,8 @@ import dev.pedrohb.certification.modules.students.useCases.VerifyHasCertificatio
 public class StudentController {
   @Autowired
   private VerifyHasCertificationUseCase verifyHasCertificationUseCase;
+  @Autowired
+  private StudentCertificationAnswersUseCase studentCertificationAnswersUseCase;
 
   @PostMapping("/verifyIfHasCertification")
   public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertificationDTO) {
@@ -27,5 +32,17 @@ public class StudentController {
     }
 
     return "Usuário pode fazer a prova";
+  }
+
+  @PostMapping("/certification/answer")
+  public ResponseEntity<Object> certificationAnswer(@RequestBody StudentCertificationAnswerDTO studentCertificationAnswerDTO) throws Exception {
+    try {
+      var result = studentCertificationAnswersUseCase.execute(studentCertificationAnswerDTO);
+
+      return ResponseEntity.ok().body(result);
+
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
   }
 }
